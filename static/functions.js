631adfbe05e4f1e12e289e4bb886ae9593f12914
Anirudh Rudraraju <anirudh@Anirudhs-MacBook-Pro.local>
2019-12-20 22:57:46 -0800

    start a basic flask server

--- static/functions.js
+++ static/functions.js
@@ -9,43 +9,43 @@ function preload(callback) {
 	}
 
 	images.board = new Image()
-	images.board.src = "./chessArt/board/" + board_theme + ".png"
+	images.board.src = "/static/images/board/" + board_theme + ".png"
 	images.board.onload = function(){onload();}
 	images.bB = new Image()
-	images.bB.src = "./chessArt/piece/" + piece_theme +"/"+ "bB.svg"
+	images.bB.src = "/static/images/piece/" + piece_theme +"/"+ "bB.svg"
 	images.bB.onload = function(){onload();}
 	images.bK = new Image()
-	images.bK.src = "./chessArt/piece/" + piece_theme +"/"+ "bK.svg"
+	images.bK.src = "/static/images/piece/" + piece_theme +"/"+ "bK.svg"
 	images.bK.onload = function(){onload();}
 	images.bN = new Image()
-	images.bN.src = "./chessArt/piece/" + piece_theme +"/"+ "bN.svg"
+	images.bN.src = "/static/images/piece/" + piece_theme +"/"+ "bN.svg"
 	images.bN.onload = function(){onload();}
 	images.bP = new Image()
-	images.bP.src = "./chessArt/piece/" + piece_theme +"/"+ "bP.svg"
+	images.bP.src = "/static/images/piece/" + piece_theme +"/"+ "bP.svg"
 	images.bP.onload = function(){onload();}
 	images.bQ = new Image()
-	images.bQ.src = "./chessArt/piece/" + piece_theme +"/"+ "bQ.svg"
+	images.bQ.src = "/static/images/piece/" + piece_theme +"/"+ "bQ.svg"
 	images.bQ.onload = function(){onload();}
 	images.bR = new Image()
-	images.bR.src = "./chessArt/piece/" + piece_theme +"/"+ "bR.svg"
+	images.bR.src = "/static/images/piece/" + piece_theme +"/"+ "bR.svg"
 	images.bR.onload = function(){onload();}
 	images.wB = new Image()
-	images.wB.src = "./chessArt/piece/" + piece_theme +"/"+ "wB.svg"
+	images.wB.src = "/static/images/piece/" + piece_theme +"/"+ "wB.svg"
 	images.wB.onload = function(){onload();}
 	images.wK = new Image()
-	images.wK.src = "./chessArt/piece/" + piece_theme +"/"+ "wK.svg"
+	images.wK.src = "/static/images/piece/" + piece_theme +"/"+ "wK.svg"
 	images.wK.onload = function(){onload();}
 	images.wN = new Image()
-	images.wN.src = "./chessArt/piece/" + piece_theme +"/"+ "wN.svg"
+	images.wN.src = "/static/images/piece/" + piece_theme +"/"+ "wN.svg"
 	images.wN.onload = function(){onload();}
 	images.wP = new Image()
-	images.wP.src = "./chessArt/piece/" + piece_theme +"/"+ "wP.svg"
+	images.wP.src = "/static/images/piece/" + piece_theme +"/"+ "wP.svg"
 	images.wP.onload = function(){onload();}
 	images.wQ = new Image()
-	images.wQ.src = "./chessArt/piece/" + piece_theme +"/"+ "wQ.svg"
+	images.wQ.src = "/static/images/piece/" + piece_theme +"/"+ "wQ.svg"
 	images.wQ.onload = function(){onload();}
 	images.wR = new Image()
-	images.wR.src = "./chessArt/piece/" + piece_theme +"/"+ "wR.svg"
+	images.wR.src = "/static/images/piece/" + piece_theme +"/"+ "wR.svg"
 	images.wR.onload = function(){onload();}
 }
 
